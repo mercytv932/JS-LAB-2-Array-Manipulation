d4ba30d62eb6398349bbfb22543ace58432c1cba
Yosef <mercytv932@gmail.com>
2026-07-23 23:39:30 -0400

    task 2 completed

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 
 let shoppingList = [];
 
-function addItem(item){
-    shoppingList.push(item);
+function addItem(item) {
+  shoppingList.push(item);
 }
 
-function removeLastItem(){
-    shoppingList.pop();
+function removeLastItem() {
+  shoppingList.pop();
 }
 
-function displayList(){
-    shoppingList.forEach(item=>{
-        console.log(item);
-    })
+function displayList() {
+  shoppingList.forEach((item) => {
+    console.log(item);
+  });
 }
 
 addItem("Apple");
@@ -26,3 +26,44 @@ console.log(shoppingList);
 
 displayList();
 
+
+
+
+//Task 2: Filter and Search an Array
+
+let shoppingList = [];
+
+function addItem(item) {
+  if (shoppingList.includes(item)) {
+    console.log("Already in the list");
+  } else {
+    shoppingList.push(item);
+  }
+}
+
+function removeLastItem() {
+  shoppingList.pop();
+}
+
+function displayList() {
+  shoppingList.forEach((item) => {
+    console.log(item);
+  });
+}
+
+function filterItems(searchTerm) {
+  const term = searchTerm.toLowerCase();
+  return shoppingList.filter((item) => item.toLowerCase().includes(term));
+}
+
+addItem("Apple");
+addItem("Orange");
+addItem("Orange");
+console.log(shoppingList);
+
+removeLastItem();
+console.log(shoppingList);
+
+displayList();
+
+addItem("Apple");
